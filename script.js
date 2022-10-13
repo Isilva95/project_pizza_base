b2bdc795525dd2de87398a60219c44548b1f7131
Isilva95 <isilva_santos@hotmail.com>
2022-10-13 08:23:34 -0300

    Atualização do Modal

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ pizzaJson.map((item, index)=> {
   // Aqui estou clonando meu .pizza-item.
   let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+  // Inserindo qual a chave daquele produto expecifico.
+  pizzaItem.setAttribute('data-key', index);
   // Preencher as informações em .pizza-item.
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -16,9 +18,18 @@ pizzaJson.map((item, index)=> {
   pizzaItem.querySelector('a').addEventListener('click',(e) => {
     e.preventDefault();
 
+    // Pegando as informações do produto.
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+    // Preenchendo as informações no Modal.
+    c('.pizzaBig img').src = pizzaJson[key].img;
+    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
     // Abrindo o modal.
     c('.pizzaWindowArea').style.opacity = 0;
     c('.pizzaWindowArea').style.display = 'flex';
+    // Aqui estou colocando um tempo para abrir o modal.
     setTimeout(() => {
       c('.pizzaWindowArea').style.opacity = 1;
     }, 200);
